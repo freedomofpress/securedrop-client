eee391f2a5658ab67d74d8f20cf7f915b2c166d8
redshiftzero <jen@freedom.press>
2018-07-30 17:22:54 -0700

    Storage layer: Add sources table

--- securedrop_client/models.py
+++ securedrop_client/models.py
@@ -2,7 +2,8 @@ import os
 import sqlite3
 import subprocess
 
-from sqlalchemy import Column, create_engine, Integer, String
+from sqlalchemy import (Boolean, Column, create_engine, DateTime, Integer,
+                        String)
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -15,6 +16,22 @@ if not os.path.exists(DB_PATH):
 
 engine = create_engine('sqlite:///{}'.format(DB_PATH))
 Base = declarative_base()
+
+
+class Source(Base):
+    __tablename__ = 'sources'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(String(36), unique=True, nullable=False)
+    journalist_designation = Column(String(255), nullable=False)
+    is_flagged = Column(Boolean, default=False)
+    public_key = Column(String(10000), nullable=True)
+    interaction_count = Column(Integer, default=0, nullable=False)
+    is_starred = Column(Boolean, default=False)
+    last_updated = Column(DateTime)
+
+    def __init__(self, uuid, journalist_designation):
+        self.uuid = uuid
+        self.journalist_designation = journalist_designation
 
 
 class User(Base):
