6656cfb6b19eb81b8c936b67b08bddf7da8cf6e6
Kushal Das <kushal@freedom.press>
2018-08-09 00:00:43 +0530

    Adds setup.py and a MANIFEST.in file
    Now we can create sdists for pypi.org.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sdclientapi",
+    version="0.1.0",
+    author="Kushal Das",
+    author_email="kushal@freedom.press",
+    description="Python client API to access SecureDrop Journalist REST API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["requests", "mypy-extensions"],
+    python_requires='>=3.5',
+    url="https://github.com/freedomofpress/sdclientapi",
+    packages=setuptools.find_packages(exclude=['docs', 'tests']),
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        'Intended Audience :: Developers',
+        "Operating System :: OS Independent",
+    ),
+)
