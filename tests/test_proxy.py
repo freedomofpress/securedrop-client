39dd7f0261a369d424b454232521033ad98c8186
redshiftzero <jen@freedom.press>
2018-08-16 16:19:07 -0700

    Unit tests: Basic tests for proxy logic

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -22,9 +22,13 @@ class TestProxy(unittest.TestCase):
         # this will be `qvm-move...` in production
         subprocess.run(["cp", fh.name, "/tmp/{}".format(self.fn)])
 
-        res.headers['X-Origin-Content-Type'] = res.headers['content-type']
+        res.headers['X-Origin-Content-Type'] = res.headers['Content-Type']
         res.headers['Content-Type'] = 'application/json'
         res.body = json.dumps({'filename': self.fn })
+
+    def on_done(self, res):
+        res.headers['X-Origin-Content-Type'] = res.headers['Content-Type']
+        res.headers['Content-Type'] = 'application/json'
 
     def test_400_if_callback_not_set(self):
         req = proxy.Req()
@@ -35,7 +39,7 @@ class TestProxy(unittest.TestCase):
         p = proxy.Proxy()
         p.proxy()
 
-        assert p.res.status == 400
+        self.assertEquals(p.res.status, 400)
 
     @vcr.use_cassette('fixtures/basic_proxy_functionality.yaml')
     def test_proxy_basic_functionality(self):
@@ -47,11 +51,21 @@ class TestProxy(unittest.TestCase):
         p = proxy.Proxy(self.conf, req, self.on_save)
         p.proxy()
 
-        assert p.res.status == 200
-        assert p.res.body == json.dumps({'filename': self.fn })
+        self.assertEquals(p.res.status, 200)
+        self.assertEquals(p.res.body, json.dumps({'filename': self.fn }))
+        self.assertEquals(p.res.headers['Content-Type'], 'application/json')
 
-        # This is the way to verify the 'content-type' header
-        assert p.res.headers['_store']['content-type'][1] == 'application/json'
+    @vcr.use_cassette('fixtures/proxy_404.yaml')
+    def test_proxy_produces_404(self):
+        req = proxy.Req()
+        req.method = 'GET'
+        req.path_query = '/notfound'
+        req.headers = {'Accept': 'application/json'}
 
-        # But I want to do this
-        # assert p.res.headers['Content-Type'] == 'application/json'
+        p = proxy.Proxy(self.conf, req)
+        p.on_save = self.on_save
+        p.on_done = self.on_done
+        p.proxy()
+
+        self.assertEquals(p.res.status, 404)
+        self.assertEquals(p.res.headers['Content-Type'], 'application/json')
