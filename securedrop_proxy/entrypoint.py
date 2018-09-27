d431cd449dddc138f1ea84a4903680b704f2ba04
Kushal Das <mail@kushaldas.in>
2018-09-27 16:25:37 -0400

    Updates entrypoint into a function

--- securedrop_proxy/entrypoint.py
+++ securedrop_proxy/entrypoint.py
@@ -8,34 +8,39 @@
 
 import sys
 import json
-import securedrop_proxy.proxy as proxy
 import uuid
 import subprocess
-import securedrop_proxy.config as config
-import securedrop_proxy.callbacks as callbacks
-import securedrop_proxy.main as main
 
-# a fresh, new proxy object
-p = proxy.Proxy()
+from . import proxy
+from . import config
+from . import callbacks
+from . import main
 
-# set up an error handler early, so we can use it during
-# configuration, etc
-p.on_done = callbacks.err_on_done
 
-# path to config file must be at argv[1]
-if len(sys.argv) != 2:
-    p.simple_error(500, 'sd-proxy script not called with path to configuration file')
-    p.on_done(p.res)
+def start():
+    # a fresh, new proxy object
+    p = proxy.Proxy()
 
-# read config. `read_conf` will call `p.on_done` if there is a config
-# problem, and will return a Conf object on success.
-conf_path = sys.argv[1]
-p.conf = config.read_conf(conf_path, p)
+    # set up an error handler early, so we can use it during
+    # configuration, etc
+    p.on_done = callbacks.err_on_done
 
-# read user request from STDIN
-incoming = []
-for line in sys.stdin:
-    incoming.append(line)
-incoming = '\n'.join(incoming)
+    # path to config file must be at argv[1]
+    if len(sys.argv) != 2:
+        p.simple_error(
+            500, "sd-proxy script not called with path to configuration file"
+        )
+        p.on_done(p.res)
 
-main.__main__(incoming, p)
+    # read config. `read_conf` will call `p.on_done` if there is a config
+    # problem, and will return a Conf object on success.
+    conf_path = sys.argv[1]
+    p.conf = config.read_conf(conf_path, p)
+
+    # read user request from STDIN
+    incoming = []
+    for line in sys.stdin:
+        incoming.append(line)
+    incoming = "\n".join(incoming)
+
+    main.__main__(incoming, p)
