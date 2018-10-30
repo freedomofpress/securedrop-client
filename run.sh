ca1c21b4ab8fd52ffc1de5d3412e18e0489d3605
Kushal Das <kushal@freedom.press>
2018-10-30 19:32:22 +0530

    Adds createdb.py and updates run.sh to work for local instance
    We are now using the similar directory structure in run.sh
    and also in securdrop-client. We are also using the createdb.py
    to create the db in the local environment.

--- run.sh
+++ run.sh
@@ -16,14 +16,20 @@ while [ -n "$1" ]; do
 done
 
 SDC_HOME=${SDC_HOME:-$(mktemp -d)}
+DB_HOME=$SDC_HOME/data
+LOGS_HOME=$SDC_HOME/logs
+
+export SDC_HOME DB_HOME LOGS_HOME
+
+mkdir -p $DB_HOME
+mkdir -p $LOGS_HOME
+chmod 0700 $SDC_HOME
+chmod 0700 $DB_HOME
+chmod 0700 $LOGS_HOME
 
 echo "Running app with home directory: $SDC_HOME"
 
 # create the database for local testing
-
-python - << EOF
-from securedrop_client.models import Base, make_engine
-Base.metadata.create_all(make_engine("$SDC_HOME"))
-EOF
+./createdb.py $DB_HOME
 
 exec python -m securedrop_client --sdc-home "$SDC_HOME" $@
