ffb46b19b8a345a48d771eef0d02a4fb03f530d4
redshiftzero <jen@freedom.press>
2018-11-05 22:30:44 -0800

    remove duplicate test

--- tests/test_message_sync.py
+++ tests/test_message_sync.py
@@ -60,41 +60,6 @@ def test_MessageSync_run_success():
         ms.run(False)
 
 
-def test_MessageSync_run_success():
-    submission = mock.MagicMock()
-    submission.download_url = "http://foo"
-    submission.filename = "foo.gpg"
-
-    fh = mock.MagicMock()
-    fh.name = "foo"
-
-    with mock.patch('securedrop_client.storage.find_new_submissions',
-                    return_value=[
-                        submission
-                    ]),\
-            mock.patch('subprocess.call',
-                       return_value=0), \
-            mock.patch('shutil.move') as mock_move, \
-            mock.patch('shutil.copy') as mock_copy, \
-            mock.patch('os.unlink') as mock_unlink, \
-            mock.patch('securedrop_client.message_sync.storage'
-                       '.mark_file_as_downloaded') as mock_mark_as_dl, \
-            mock.patch(
-                'tempfile.NamedTemporaryFile',
-                return_value=fh) as mock_temp_file, \
-            mock.patch('builtins.open', mock.mock_open(read_data="blah")):
-
-        api = mock.MagicMock()
-        home = "/home/user/.sd"
-
-        ms = MessageSync(api, home)
-        ms.api.download_submission = mock.MagicMock(
-            return_value=(1234, "/home/user/downloads/foo")
-        )
-
-        ms.run(False)
-
-
 def test_MessageSync_exception():
     """
     Mostly here for code coverage- makes sure that if an exception is
