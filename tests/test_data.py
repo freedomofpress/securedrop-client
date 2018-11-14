21b574381cbeb0ecf19d85f45e2de0c1c225a130
redshiftzero <jen@freedom.press>
2018-11-13 16:15:04 -0800

    data.py: Gracefully handle missing files on disk
    So, I can modify gui/main.py, but I'd need to refresh the source
    object which requires access to the current session.
    
    I think the cleanest thing to do here is to make sure that if the
    file is missing on disk, then the application doesn't crash, and
    instead the message should indicate that it's been deleted.

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,13 +2,25 @@
 Make sure the Data object (which abstracts access to the `/data/`
 storage directory) works as expected.
 """
-
 from securedrop_client.data import Data
 
 
-def test_Data_init(tmpdir):
+def test_Data_init(safe_tmpdir):
     """
     Ensure the Data class instantiates as expected
     """
-    data = Data(tmpdir)
-    assert data.data_dir == tmpdir
+    data = Data(str(safe_tmpdir))
+    assert data.data_dir == safe_tmpdir
+
+
+def test_Data_file_not_found(safe_tmpdir):
+    """
+    Ensure the data class handles missing files gracefully
+    (e.g. if the file gets deleted)
+    """
+    data = Data(str(safe_tmpdir))
+    msg = data.get('my-file.gpg')
+
+    # No unhandled exceptions should occur, and a 'nice' msg
+    # should be shown.
+    assert msg == "<Content deleted>"
