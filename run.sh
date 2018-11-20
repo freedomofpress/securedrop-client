da3045c5fc8f52da6299b8da57193c52523714b2
heartsucker <heartsucker@autistici.org>
2018-11-20 20:39:56 +0100

    added dev gpg keys, import at app start

--- run.sh
+++ run.sh
@@ -19,11 +19,16 @@ SDC_HOME=${SDC_HOME:-$(mktemp -d)}
 
 export SDC_HOME
 
-chmod 0700 $SDC_HOME
+GPG_HOME="$SDC_HOME/gpg"
+mkdir "$GPG_HOME"
+chmod 0700 "$SDC_HOME" "$GPG_HOME"
 
 echo "Running app with home directory: $SDC_HOME"
+echo ""
+
+gpg --homedir "$GPG_HOME" --allow-secret-key-import --import tests/files/securedrop.gpg.asc
 
 # create the database for local testing
-./createdb.py $SDC_HOME
+./createdb.py "$SDC_HOME"
 
 exec python -m securedrop_client --sdc-home "$SDC_HOME" --no-proxy $@
