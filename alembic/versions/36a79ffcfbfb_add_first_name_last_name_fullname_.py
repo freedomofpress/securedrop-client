ad0fff49e1eee90a06bb6712faf50734b3238d56
Allie Crevier <allie@freedom.press>
2019-07-01 17:58:35 -0700

    update down_revision to original_filename migration

--- alembic/versions/36a79ffcfbfb_add_first_name_last_name_fullname_.py
+++ alembic/versions/36a79ffcfbfb_add_first_name_last_name_fullname_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '36a79ffcfbfb'
-down_revision = 'fecf1191b6f0'
+down_revision = 'bafdcae12f97'
 branch_labels = None
 depends_on = None
 
