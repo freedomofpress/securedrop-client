6530b0c63c5e40cfa9d4649b3d186ef3fabb7880
Kushal Das <mail@kushaldas.in>
2019-11-01 17:57:46 +0530

    Adds a journalctl example
    The log file is actually not storing all messages.

---
+++ journal-example.py
@@ -0,0 +1,26 @@
+import logging
+from oqubeslogging import OQubesLog
+from systemd import journal
+import select
+
+
+def main():
+    handler = OQubesLog("workvm", "logging")
+    logging.basicConfig(level=logging.DEBUG, handlers=[handler])
+    logger = logging.getLogger("example")
+    j = journal.Reader()
+    j.seek_tail()
+
+    p = select.poll()
+    p.register(j, j.get_events())
+    while True:    
+        p.poll()
+        if j.process() == journal.APPEND:
+            for m in j:
+                msg = "MSG: {}".format(m["MESSAGE"])
+                print(msg)
+                # TODO: Figure out why the log file in the logging VM is closing 
+                logger.info(m["MESSAGE"])
+
+if __name__ == "__main__":
+    main()
