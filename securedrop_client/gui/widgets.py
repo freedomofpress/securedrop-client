626fb2ffd6fc350b9518f1f9e9a8c6bfea639702
deeplow <deeplow@tutanota.com>
2019-11-13 14:46:42 -0500

    close #331 fix bug of sources disappearing from source list
    workaround: do styling on border instead of in the background and
    make border very large so that the default style is shrinked until
    it no longer exists.
    
    See https://github.com/freedomofpress/securedrop-client/issues/331
    for discussion

--- securedrop_client/gui/widgets.py
+++ securedrop_client/gui/widgets.py
@@ -834,14 +834,16 @@ class SourceList(QListWidget):
     Displays the list of sources.
     """
 
+    # Thick 500px border in QListView::item:selected is a workaround.
+    # See https://github.com/freedomofpress/securedrop-client/issues/331
     CSS = '''
     QListView {
         border: none;
-        show-decoration-selected: 0;
+        show-decoration-selected: 1;
         border-right: 3px solid #f3f5f9;
     }
     QListView::item:selected {
-        background: #f3f5f9;
+        border: 500px solid #f3f5f9;
     }
     '''
 
