de75f9a90e2d2ebc8c9e60e09fd92c43d227c865
redshiftzero <jen@freedom.press>
2020-03-09 21:00:41 -0400

    securedrop-client 0.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for name in os.listdir('./securedrop_client/resources/images/'):
 
 setuptools.setup(
     name="securedrop-client",
-    version="0.1.1",
+    version="0.1.2",
     author="Freedom of the Press Foundation",
     author_email="securedrop@freedom.press",
     description="SecureDrop Workstation client application",
