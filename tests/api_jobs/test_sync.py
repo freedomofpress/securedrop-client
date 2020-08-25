5c6a8685314fd6549fd2cf6fdcba1dca4063d4e4
Allie Crevier <allie@freedom.press>
2020-08-25 10:23:13 -0700

    update current user during sync

--- tests/api_jobs/test_sync.py
+++ tests/api_jobs/test_sync.py
@@ -18,9 +18,37 @@ def test_MetadataSyncJob_success(mocker, homedir, session, session_maker):
         "securedrop_client.api_jobs.sync.get_remote_data", return_value=([mock_source], [], [])
     )
 
-    api_client = mocker.MagicMock()
-    api_client.default_request_timeout = mocker.MagicMock()
-    api_client.default_request_timeout = mocker.MagicMock()
+    user = factory.User(uuid="mock1", username="mock1", firstname="mock1", lastname="mock1")
+    session.add(user)
+
+    api_client = mocker.patch("securedrop_client.logic.sdclientapi.API")
+
+    user = {"uuid": "mock1", "username": "mock1", "first_name": "mock1", "last_name": "mock1"}
+    mocker.patch.object(api_client, "get_current_user", return_value=user)
+
+    job.call_api(api_client, session)
+
+    assert mock_get_remote_data.call_count == 1
+
+
+def test_MetadataSyncJob_success_current_user_name_change(mocker, homedir, session, session_maker):
+    job = MetadataSyncJob(homedir)
+
+    mock_source = factory.RemoteSource(
+        key={"type": "PGP", "public": PUB_KEY, "fingerprint": "123456ABC"}
+    )
+
+    mock_get_remote_data = mocker.patch(
+        "securedrop_client.api_jobs.sync.get_remote_data", return_value=([mock_source], [], [])
+    )
+
+    user = factory.User(uuid="mock1", username="mock1", firstname="mock1", lastname="mock1")
+    session.add(user)
+
+    api_client = mocker.patch("securedrop_client.logic.sdclientapi.API")
+
+    user = {"uuid": "mock2", "username": "mock2", "first_name": "mock2", "last_name": "mock2"}
+    mocker.patch.object(api_client, "get_current_user", return_value=user)
 
     job.call_api(api_client, session)
 
