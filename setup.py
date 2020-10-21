9d7fb42a510631d8eb41632e72160d82b90f264a
Allie Crevier <allie@freedom.press>
2020-10-21 09:51:56 -0700

    securedrop-sdk 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="securedrop-sdk",
-    version="0.1.1",
+    version="0.2.0",
     author="Freedom of the Press Foundation",
     author_email="securedrop@freedom.press",
     description="Python client API to access SecureDrop Journalist REST API",
