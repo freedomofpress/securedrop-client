3d8da2927b6b7b1095fb66eadb25974c25c09dee
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-01-25 19:39:38 +1100

    Add component to hold the application state
    Except for user actions (QAction), the GUI widgets should not
    need more than read-only access to the application state.
    
    The user actions should become the interface for the GUI to
    modify the application state.
    
    On properties, see https://realpython.com/python-property

---
+++ securedrop_client/state/domain.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright © 2022 The Freedom of the Press Foundation.
+"""
+The types relevant to the internal state of the SecureDrop Client.
+"""
+from typing import NewType
+
+
+class SourceId(str):
+    """Identifies a source."""
+
+    pass
+
+
+ConversationId = NewType("ConversationId", str)
+
+
+class FileId(str):
+    """Identifies a file."""
+
+    pass
+
+
+class File:
+    def __init__(self, id: FileId) -> None:
+        self._id: str = id
+        self._is_downloaded: bool = False
+
+    @property
+    def id(self) -> str:
+        """A unique identifier file set by the server (opaque string)."""
+        return self._id
+
+    @property
+    def is_downloaded(self) -> bool:
+        """Whether the file is available locally."""
+        return self._is_downloaded
+
+    @is_downloaded.setter
+    def is_downloaded(self, value: bool) -> None:
+        self._is_downloaded = value
