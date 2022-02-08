52b439aa7ec8f7ffb563416673fadbda8dbbaa74
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-02-08 19:22:53 +1100

    Add tests for actions.DeleteSource

--- tests/gui/test_actions.py
+++ tests/gui/test_actions.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QDialog, QMenu
 
 from securedrop_client.db import Source
-from securedrop_client.gui.actions import DeleteConversationAction
+from securedrop_client.gui.actions import DeleteConversationAction, DeleteSourceAction
 from securedrop_client.logic import Controller
 from tests import factory
 
@@ -45,3 +45,36 @@ class DeleteConversationActionTest(unittest.TestCase):
         self.action.trigger()
 
         assert not self._controller.delete_conversation.called
+
+
+class DeleteSourceActionTest(unittest.TestCase):
+    def setUp(self):
+        self._source = factory.Source()
+        _menu = QMenu()
+        self._controller = MagicMock(Controller, api=True)
+        self._dialog = QDialog()
+
+        def _dialog_constructor(source: Source) -> QDialog:
+            return self._dialog
+
+        self.action = DeleteSourceAction(self._source, _menu, self._controller, _dialog_constructor)
+
+    def test_deletes_conversation_when_dialog_accepted(self):
+        # Accept the confimation dialog from a separate thread.
+        timer = QTimer()
+        timer.start(10)
+        timer.timeout.connect(lambda: self._dialog.accept())
+
+        self.action.trigger()
+
+        self._controller.delete_source.assert_called_once_with(self._source)
+
+    def test_does_not_delete_conversation_when_dialog_rejected(self):
+        # Reject the confimation dialog from a separate thread.
+        timer = QTimer()
+        timer.start(10)
+        timer.timeout.connect(lambda: self._dialog.reject())
+
+        self.action.trigger()
+
+        assert not self._controller.delete_source.called
