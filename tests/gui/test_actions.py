37e2c8941d26d95923929507d8e69862b22e8019
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-03-17 11:55:41 +1100

    Increase scope of DeleteConversationAction tests

--- tests/gui/test_actions.py
+++ tests/gui/test_actions.py
@@ -1,16 +1,20 @@
 import unittest
 from unittest import mock
+from unittest.mock import MagicMock
 
-from PyQt5.QtWidgets import QApplication, QMenu
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QDialog, QMenu
 
 from securedrop_client import state
+from securedrop_client.db import Source
 from securedrop_client.gui.actions import (
     DeleteConversationAction,
     DeleteSourceAction,
     DownloadConversation,
 )
-from securedrop_client.gui.conversation import DeleteConversationDialog
 from securedrop_client.gui.source import DeleteSourceDialog
+from securedrop_client.logic import Controller
+from tests import factory
 
 app = QApplication([])
 
@@ -36,35 +40,6 @@ def test_DeleteSourceAction_trigger(mocker):
     mock_delete_source_dialog_instance.exec.assert_called_once()
 
 
-def test_DeleteConversationAction_trigger(mocker):
-    mock_controller = mocker.MagicMock()
-    mock_source = mocker.MagicMock()
-    mock_delete_conversation_dialog_instance = mocker.MagicMock(DeleteConversationDialog)
-    mock_delete_conversation_dialog = mocker.MagicMock()
-    mock_delete_conversation_dialog.return_value = mock_delete_conversation_dialog_instance
-
-    delete_conversation_action = DeleteConversationAction(
-        mock_source, None, mock_controller, mock_delete_conversation_dialog
-    )
-    delete_conversation_action.trigger()
-    mock_delete_conversation_dialog_instance.exec.assert_called_once()
-
-
-def test_DeleteConversationAction_trigger_when_user_is_loggedout(mocker):
-    mock_controller = mocker.MagicMock()
-    mock_controller.api = None
-    mock_source = mocker.MagicMock()
-    mock_delete_conversation_dialog_instance = mocker.MagicMock(DeleteConversationDialog)
-    mock_delete_conversation_dialog = mocker.MagicMock()
-    mock_delete_conversation_dialog.return_value = mock_delete_conversation_dialog_instance
-
-    delete_conversation_action = DeleteConversationAction(
-        mock_source, None, mock_controller, mock_delete_conversation_dialog
-    )
-    delete_conversation_action.trigger()
-    mock_delete_conversation_dialog_instance.exec.assert_not_called()
-
-
 def test_DeleteSourceAction_requires_an_authenticated_journalist(mocker):
     mock_controller = mocker.MagicMock()
     mock_controller.api = None  # no aouthenticated journalist
@@ -79,6 +54,55 @@ def test_DeleteSourceAction_requires_an_authenticated_journalist(mocker):
     delete_source_action.trigger()
     assert not mock_delete_source_dialog_instance.exec.called
     mock_controller.on_action_requiring_login.assert_called_once()
+
+
+class DeleteConversationActionTest(unittest.TestCase):
+    def setUp(self):
+        self._source = factory.Source()
+        _menu = QMenu()
+        self._controller = MagicMock(Controller, api=True)
+        self._dialog = QDialog()
+
+        def _dialog_constructor(source: Source) -> QDialog:
+            return self._dialog
+
+        self.action = DeleteConversationAction(
+            self._source, _menu, self._controller, _dialog_constructor
+        )
+
+    def test_deletes_conversation_when_dialog_accepted(self):
+        # Accept the confimation dialog from a separate thread.
+        timer = QTimer()
+        timer.start(10)
+        timer.timeout.connect(lambda: self._dialog.accept())
+
+        self.action.trigger()
+
+        self._controller.delete_conversation.assert_called_once_with(self._source)
+
+    def test_does_not_delete_conversation_when_dialog_rejected(self):
+        # Reject the confimation dialog from a separate thread.
+        timer = QTimer()
+        timer.start(10)
+        timer.timeout.connect(lambda: self._dialog.reject())
+
+        self.action.trigger()
+
+        assert not self._controller.delete_conversation.called
+
+    def test_requires_authenticated_journalist(self):
+        controller = mock.MagicMock()
+        controller.api = None  # no authenticated user
+        self.action.controller = controller
+
+        confirmation_dialog = mock.MagicMock()
+        self.action._confirmation_dialog = confirmation_dialog
+
+        self.action.trigger()
+
+        assert not confirmation_dialog.exec.called
+        assert not controller.delete_conversation.called
+        controller.on_action_requiring_login.assert_called_once()
 
 
 class TestDownloadConversation(unittest.TestCase):
