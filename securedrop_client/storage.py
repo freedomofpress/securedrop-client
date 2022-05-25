29398d86f9e5ee955444c0f6d3a38c2decd8b746
Cory Francis Myers <cory@freedom.press>
2022-05-24 22:54:25 -0700

    fix(mark_all_pending_drafts_as_failed): filter for replies pending under previous PIDs
    If a DraftReply was marked "pending" by a previous Client session (i.e.,
    with a different PID), by now it must have either failed or been sent
    successfully.  To avoid leaving it an an inconsistent state, we presume
    it failed; we'll update it anyway if the Journalist API reports it
    received on the next sync.

--- securedrop_client/storage.py
+++ securedrop_client/storage.py
@@ -1079,6 +1079,9 @@ def get_reply(session: Session, uuid: str) -> Reply:
 
 
 def mark_all_pending_drafts_as_failed(session: Session) -> List[DraftReply]:
+    """
+    Mark as failed those pending replies that originate from other sessions (PIDs).
+    """
     pending_status = (
         session.query(ReplySendStatus).filter_by(name=ReplySendStatusCodes.PENDING.value).one()
     )
@@ -1086,7 +1089,15 @@ def mark_all_pending_drafts_as_failed(session: Session) -> List[DraftReply]:
         session.query(ReplySendStatus).filter_by(name=ReplySendStatusCodes.FAILED.value).one()
     )
 
-    pending_drafts = session.query(DraftReply).filter_by(send_status=pending_status).all()
+    pending_drafts = (
+        session.query(DraftReply)
+        .filter(
+            DraftReply.send_status == pending_status,
+            DraftReply.sending_pid.isnot(os.getpid()),
+        )
+        .all()
+    )
+    logger.debug(f"Found {len(pending_drafts)} pending replies not being processed in this session")
     for pending_draft in pending_drafts:
         pending_draft.send_status = failed_status
 
