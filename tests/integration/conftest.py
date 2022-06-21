1794e350646985fd732aaa7f9934fbc8fd1132ba
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-06-21 10:24:34 +1000

    Ensure threads are managed in integration tests
    As done previously for unit and functional tests, this commit
    ensures that the threads used by the application are closed
    before the corresponding QThread instances are destroyed.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from PyQt5.QtWidgets import QApplication
 
+from securedrop_client.app import threads
 from securedrop_client.gui import conversation
 from securedrop_client.gui.base import ModalDialog
 from securedrop_client.gui.main import Window
@@ -15,36 +16,48 @@ def main_window(mocker, homedir):
     gui = Window()
     app.setActiveWindow(gui)
     gui.show()
-    controller = Controller("http://localhost", gui, mocker.MagicMock(), homedir, None, proxy=False)
-    controller.authenticated_user = factory.User()
-    controller.qubes = False
-    gui.setup(controller)
+    with threads(4) as [export_thread, sync_thread, main_queue_thread, file_download_thread]:
+        controller = Controller(
+            "http://localhost",
+            gui,
+            mocker.MagicMock(),
+            homedir,
+            None,
+            export_thread=export_thread,
+            sync_thread=sync_thread,
+            main_queue_thread=main_queue_thread,
+            file_download_queue_thread=file_download_thread,
+            proxy=False,
+        )
+        controller.authenticated_user = factory.User()
+        controller.qubes = False
+        gui.setup(controller)
 
-    # Create a source widget
-    source_list = gui.main_view.source_list
-    source = factory.Source()
-    source_list.update([source])
+        # Create a source widget
+        source_list = gui.main_view.source_list
+        source = factory.Source()
+        source_list.update([source])
 
-    # Create a file widget, message widget, and reply widget
-    mocker.patch("securedrop_client.gui.widgets.humanize_filesize", return_value="100")
-    mocker.patch(
-        "securedrop_client.gui.base.SecureQLabel.get_elided_text",
-        return_value="1-yellow-doc.gz.gpg",
-    )
-    source.collection.append(
-        [
-            factory.File(source=source, filename="1-yellow-doc.gz.gpg"),
-            factory.Message(source=source, filename="2-yellow-msg.gpg"),
-            factory.Reply(source=source, filename="3-yellow-reply.gpg"),
-        ]
-    )
-    source_list.setCurrentItem(source_list.item(0))
-    gui.main_view.on_source_changed()
+        # Create a file widget, message widget, and reply widget
+        mocker.patch("securedrop_client.gui.widgets.humanize_filesize", return_value="100")
+        mocker.patch(
+            "securedrop_client.gui.base.SecureQLabel.get_elided_text",
+            return_value="1-yellow-doc.gz.gpg",
+        )
+        source.collection.append(
+            [
+                factory.File(source=source, filename="1-yellow-doc.gz.gpg"),
+                factory.Message(source=source, filename="2-yellow-msg.gpg"),
+                factory.Reply(source=source, filename="3-yellow-reply.gpg"),
+            ]
+        )
+        source_list.setCurrentItem(source_list.item(0))
+        gui.main_view.on_source_changed()
 
-    yield gui
+        yield gui
 
-    # Teardown
-    gui.login_dialog.close()
+        # Teardown
+        gui.login_dialog.close()
     gui.close()
     app.exit()
 
@@ -56,36 +69,48 @@ def main_window_no_key(mocker, homedir):
     gui = Window()
     app.setActiveWindow(gui)
     gui.show()
-    controller = Controller("http://localhost", gui, mocker.MagicMock(), homedir, None, proxy=False)
-    controller.authenticated_user = factory.User()
-    controller.qubes = False
-    gui.setup(controller)
+    with threads(4) as [export_thread, sync_thread, main_queue_thread, file_download_thread]:
+        controller = Controller(
+            "http://localhost",
+            gui,
+            mocker.MagicMock(),
+            homedir,
+            None,
+            export_thread=export_thread,
+            sync_thread=sync_thread,
+            main_queue_thread=main_queue_thread,
+            file_download_queue_thread=file_download_thread,
+            proxy=False,
+        )
+        controller.authenticated_user = factory.User()
+        controller.qubes = False
+        gui.setup(controller)
 
-    # Create a source widget
-    source_list = gui.main_view.source_list
-    source = factory.Source(public_key=None)
-    source_list.update([source])
+        # Create a source widget
+        source_list = gui.main_view.source_list
+        source = factory.Source(public_key=None)
+        source_list.update([source])
 
-    # Create a file widget, message widget, and reply widget
-    mocker.patch("securedrop_client.gui.widgets.humanize_filesize", return_value="100")
-    mocker.patch(
-        "securedrop_client.gui.base.SecureQLabel.get_elided_text",
-        return_value="1-yellow-doc.gz.gpg",
-    )
-    source.collection.append(
-        [
-            factory.File(source=source, filename="1-yellow-doc.gz.gpg"),
-            factory.Message(source=source, filename="2-yellow-msg.gpg"),
-            factory.Reply(source=source, filename="3-yellow-reply.gpg"),
-        ]
-    )
-    source_list.setCurrentItem(source_list.item(0))
-    gui.main_view.on_source_changed()
+        # Create a file widget, message widget, and reply widget
+        mocker.patch("securedrop_client.gui.widgets.humanize_filesize", return_value="100")
+        mocker.patch(
+            "securedrop_client.gui.base.SecureQLabel.get_elided_text",
+            return_value="1-yellow-doc.gz.gpg",
+        )
+        source.collection.append(
+            [
+                factory.File(source=source, filename="1-yellow-doc.gz.gpg"),
+                factory.Message(source=source, filename="2-yellow-msg.gpg"),
+                factory.Reply(source=source, filename="3-yellow-reply.gpg"),
+            ]
+        )
+        source_list.setCurrentItem(source_list.item(0))
+        gui.main_view.on_source_changed()
 
-    yield gui
+        yield gui
 
-    # Teardown
-    gui.login_dialog.close()
+        # Teardown
+        gui.login_dialog.close()
     gui.close()
     app.exit()
 
@@ -96,16 +121,28 @@ def modal_dialog(mocker, homedir):
     gui = Window()
     app.setActiveWindow(gui)
     gui.show()
-    controller = Controller("http://localhost", gui, mocker.MagicMock(), homedir, None, proxy=False)
-    controller.authenticated_user = factory.User()
-    controller.qubes = False
-    gui.setup(controller)
-    gui.login_dialog.close()
-    dialog = ModalDialog()
+    with threads(4) as [export_thread, sync_thread, main_queue_thread, file_download_thread]:
+        controller = Controller(
+            "http://localhost",
+            gui,
+            mocker.MagicMock(),
+            homedir,
+            None,
+            export_thread=export_thread,
+            sync_thread=sync_thread,
+            main_queue_thread=main_queue_thread,
+            file_download_queue_thread=file_download_thread,
+            proxy=False,
+        )
+        controller.authenticated_user = factory.User()
+        controller.qubes = False
+        gui.setup(controller)
+        gui.login_dialog.close()
+        dialog = ModalDialog()
 
-    yield dialog
+        yield dialog
 
-    dialog.close()
+        dialog.close()
     gui.close()
     app.exit()
 
@@ -116,17 +153,32 @@ def print_dialog(mocker, homedir):
     gui = Window()
     app.setActiveWindow(gui)
     gui.show()
-    controller = Controller("http://localhost", gui, mocker.MagicMock(), homedir, None, proxy=False)
-    controller.authenticated_user = factory.User()
-    controller.qubes = False
-    export_device = conversation.ExportDevice(controller)
-    gui.setup(controller)
-    gui.login_dialog.close()
-    dialog = conversation.PrintFileDialog(export_device, "file_uuid", "file_name")
+    with threads(4) as [export_thread, sync_thread, main_queue_thread, file_download_thread]:
+        controller = Controller(
+            "http://localhost",
+            gui,
+            mocker.MagicMock(),
+            homedir,
+            None,
+            export_thread=export_thread,
+            sync_thread=sync_thread,
+            main_queue_thread=main_queue_thread,
+            file_download_queue_thread=file_download_thread,
+            proxy=False,
+        )
+        controller = Controller(
+            "http://localhost", gui, mocker.MagicMock(), homedir, None, proxy=False
+        )
+        controller.authenticated_user = factory.User()
+        controller.qubes = False
+        export_device = conversation.ExportDevice(controller)
+        gui.setup(controller)
+        gui.login_dialog.close()
+        dialog = conversation.PrintFileDialog(export_device, "file_uuid", "file_name")
 
-    yield dialog
+        yield dialog
 
-    dialog.close()
+        dialog.close()
     gui.close()
     app.exit()
 
@@ -137,17 +189,29 @@ def export_dialog(mocker, homedir):
     gui = Window()
     app.setActiveWindow(gui)
     gui.show()
-    controller = Controller("http://localhost", gui, mocker.MagicMock(), homedir, None, proxy=False)
-    controller.authenticated_user = factory.User()
-    controller.qubes = False
-    export_device = conversation.ExportDevice(controller)
-    gui.setup(controller)
-    gui.login_dialog.close()
-    dialog = conversation.ExportFileDialog(export_device, "file_uuid", "file_name")
-    dialog.show()
+    with threads(4) as [export_thread, sync_thread, main_queue_thread, file_download_thread]:
+        controller = Controller(
+            "http://localhost",
+            gui,
+            mocker.MagicMock(),
+            homedir,
+            None,
+            export_thread=export_thread,
+            sync_thread=sync_thread,
+            main_queue_thread=main_queue_thread,
+            file_download_queue_thread=file_download_thread,
+            proxy=False,
+        )
+        controller.authenticated_user = factory.User()
+        controller.qubes = False
+        export_device = conversation.ExportDevice(controller)
+        gui.setup(controller)
+        gui.login_dialog.close()
+        dialog = conversation.ExportFileDialog(export_device, "file_uuid", "file_name")
+        dialog.show()
 
-    yield dialog
+        yield dialog
 
-    dialog.close()
+        dialog.close()
     gui.close()
     app.exit()
