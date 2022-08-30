7d06d0ef8fb23bcf54770e23a1884e1f05a6ebf3
Cory Francis Myers <cory@freedom.press>
2022-08-29 18:15:46 -0700

    feat(MetadataSyncJob): override API.default_request_timeout if $SDEXTENDEDTIMEOUT is set

--- securedrop_client/api_jobs/sync.py
+++ securedrop_client/api_jobs/sync.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import Any, List, Optional
 
 from sdclientapi import API
@@ -41,7 +42,15 @@ class MetadataSyncJob(ApiJob):
         #
         # This timeout is used for 3 different requests: `get_sources`, `get_all_submissions`, and
         # `get_all_replies`
-        api_client.default_request_timeout = self.DEFAULT_REQUEST_TIMEOUT
+        api_client.default_request_timeout = int(
+            os.environ.get("SDEXTENDEDTIMEOUT", self.DEFAULT_REQUEST_TIMEOUT)
+        )
+        if api_client.default_request_timeout != self.DEFAULT_REQUEST_TIMEOUT:
+            logger.warn(
+                f"{self.__class__.__name__} will use "
+                f"default_request_timeout={api_client.default_request_timeout}"
+            )
+
         users = api_client.get_users()
         MetadataSyncJob._update_users(session, users)
         sources, submissions, replies = get_remote_data(api_client)
