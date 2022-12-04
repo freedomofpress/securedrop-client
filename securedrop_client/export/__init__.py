3d2d31046123ddaa675f082513427fc2ffe1bbc7
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-12-05 01:30:49 +1100

    Refactor remove unused export.Service alias
    I am leaving the deprecated tests as untouched as possible to make
    easier to review the changes.

--- securedrop_client/export/__init__.py
+++ securedrop_client/export/__init__.py
@@ -1,3 +1,3 @@
 from .cli import Error as ExportError  # noqa: F401
 from .cli import Status as ExportStatus  # noqa: F401
-from .service import Export, Service  # noqa: F401
+from .service import Service  # noqa: F401
