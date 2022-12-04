f5ffdb953f2c37fb46a8891f035e7f1e13002ef9
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-12-04 13:02:35 +1100

    Add export.Printer

--- securedrop_client/export/__init__.py
+++ securedrop_client/export/__init__.py
@@ -1,3 +1,4 @@
 from .cli import Error as ExportError  # noqa: F401
 from .cli import Status as ExportStatus  # noqa: F401
+from .printer import Printer, getPrinter  # noqa: F401
 from .service import Service, getService  # noqa: F401
