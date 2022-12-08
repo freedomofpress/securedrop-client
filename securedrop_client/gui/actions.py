026cb48a695e64e05a428050dceb411ec0b525c8
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-12-08 17:18:06 +1100

    Add actions.PrintConversation (missing tests)

--- securedrop_client/gui/actions.py
+++ securedrop_client/gui/actions.py
@@ -4,15 +4,20 @@ The actions available to the journalist.
 Over time, this module could become the interface between
 the GUI and the controller.
 """
+import logging
 from gettext import gettext as _
+from pathlib import Path
 from typing import Callable, Optional
 
-from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QAction, QDialog, QMenu
 
-from securedrop_client import state
+from securedrop_client import conversation, export, state
 from securedrop_client.db import Source
 from securedrop_client.logic import Controller
+from securedrop_client.utils import safe_mkdir
+
+logger = logging.getLogger(__name__)
 
 
 class DownloadConversation(QAction):
@@ -126,3 +131,113 @@ class DeleteConversation(QAction):
                 return
             self.controller.delete_conversation(self.source)
             self._state.remove_conversation_files(id)
+
+
+class PrintConversation(QAction):
+    """Use this action to print a transcript of the messages, replies, etc.
+
+    The transcript includes references to any attached files.
+    """
+
+    printer_job_enqueued = pyqtSignal(list)
+
+    def __init__(
+        self,
+        source: Source,
+        parent: QMenu,
+        controller: Controller,
+        confirmation_dialog: Callable[[export.Printer, str], QDialog],
+        error_dialog: Callable[[str, str], QDialog],
+    ) -> None:
+        title = _("Print Conversation")
+        super().__init__(title, parent)
+
+        self._controller = controller
+        self._source = source
+        printing_service = export.getService()
+        self._printer = export.getPrinter(printing_service)
+        self._create_error_dialog = error_dialog
+        self._create_confirmation_dialog = confirmation_dialog
+        self._printing_job_id = self._source.journalist_designation
+        self._printing_job_failure_notification_in_progress = False
+
+        self.setShortcut(Qt.CTRL + Qt.Key_P)
+
+        self._printer.job_failed.connect(self._on_printing_job_failed)
+        self._printer.job_done.connect(self._on_printing_job_done)
+        self.triggered.connect(self.trigger)
+
+        self._printer.enqueue_job_on(self.printer_job_enqueued)
+
+    @pyqtSlot()
+    def trigger(self) -> None:
+        if self._controller.api is None:
+            self._controller.on_action_requiring_login()
+        else:
+            self._printer.connect()
+            self._confirmation_dialog = self._create_confirmation_dialog(
+                self._printer, self._transcript_display_name
+            )
+            self._confirmation_dialog.accepted.connect(self._on_confirmation_dialog_accepted)
+            self._confirmation_dialog.rejected.connect(self._on_confirmation_dialog_rejected)
+            self._confirmation_dialog.finished.connect(self._printer.disconnect)
+            self._confirmation_dialog.show()
+
+    @pyqtSlot()
+    def _on_confirmation_dialog_rejected(self) -> None:
+        self.setEnabled(True)
+        self._confirmation_dialog.deleteLater()
+
+    @pyqtSlot()
+    def _on_confirmation_dialog_accepted(self) -> None:
+        self.setEnabled(False)
+        self._enqueue_printing_job(self._transcript_path)
+        self._confirmation_dialog.deleteLater()
+
+    @pyqtSlot(str)
+    def _on_printing_job_done(self, job_id: str) -> None:
+        self.setEnabled(True)
+
+    @pyqtSlot(str, str)
+    def _on_printing_job_failed(self, job_id: str, reason: str) -> None:
+        self._error_dialog = self._create_error_dialog(job_id, reason)
+        self._error_dialog.finished.connect(self._on_error_dialog_finished)
+        self._error_dialog.finished.connect(self._printer.disconnect)
+        self._error_dialog.show()
+
+    @pyqtSlot(int)
+    def _on_error_dialog_finished(self, _result: int) -> None:
+        self.setEnabled(True)
+        self._error_dialog.deleteLater()
+
+    def _start_printer(self) -> None:
+        """Start the printer in a thread-safe manner."""
+        self.printer_start_requested.emit()
+
+    def _enqueue_printing_job(self, file_path: Path) -> None:
+        """Enqueue a printing job in a thread-safe manner."""
+
+        transcript = conversation.Transcript(self._source)
+        safe_mkdir(file_path.parent)
+
+        with open(file_path, "w") as f:
+            f.write(str(transcript))
+            logger.info(f"Printing transcript of conversation: ({self._transcript_display_name})")
+            self.printer_job_enqueued.emit([str(file_path)])
+
+    @property
+    def _transcript_path(self) -> Path:
+        """The transcript path. This is te source of truth for this data."""
+        return (
+            Path(self._controller.data_dir)
+            .joinpath(self._source.journalist_filename)
+            .joinpath("conversation.txt")
+        )
+
+    @property
+    def _transcript_display_name(self) -> str:
+        """The transcript name for display purposes.
+
+        Example: wonderful_source/conversation.txt
+        """
+        return str(self._transcript_path.relative_to(self._transcript_path.parents[1]))
--- tests/gui/test_actions.py
+++ tests/gui/test_actions.py
@@ -1,12 +1,15 @@
 import unittest
+from tempfile import TemporaryDirectory
 from unittest import mock
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from PyQt5.QtCore import QTimer
+from PyQt5.QtTest import QSignalSpy
 from PyQt5.QtWidgets import QDialog, QMenu
 
 from securedrop_client import state
 from securedrop_client.db import Source
+from securedrop_client.export import Printer
 from securedrop_client.gui import actions
 from securedrop_client.logic import Controller
 from tests import factory
@@ -241,3 +244,79 @@ class TestDownloadConversation(unittest.TestCase):
         action.setEnabled(False)
         action._on_selected_conversation_files_changed()
         assert not action.isEnabled()
+
+
+MAX_SIGNAL_WAIT_TIME = 20  # milliseconds
+
+
+class PrintConversationTest(unittest.TestCase):
+    @patch("securedrop_client.export.getPrinter")
+    @patch("securedrop_client.export.getService")
+    def setUp(self, getService, getPrinter):
+        _source = MagicMock(
+            factory.Source,
+            journalist_designation="tentative source",
+            journalist_filename="tentative-source",
+            collection=[],
+        )
+        self._menu = QMenu()
+        self._controller = MagicMock(Controller, api=True)
+        self._confirmation_dialog = QDialog()
+        self._error_dialog = QDialog()
+
+        def _confirmation_dialog_constructor(
+            printer: Printer, transcript_display_name: str
+        ) -> QDialog:
+            return self._confirmation_dialog
+
+        def _error_dialog_constructor(source: Source) -> QDialog:
+            return self._error_dialog
+
+        self.action = actions.PrintConversation(
+            _source,
+            self._menu,
+            self._controller,
+            _confirmation_dialog_constructor,
+            _error_dialog_constructor,
+        )
+
+    def test_enqueues_conversation_transcript_for_printing_when_confirmation_dialog_accepted(self):
+        with TemporaryDirectory() as tmpdir:
+            self._controller.data_dir = tmpdir
+            expected_transcript_path = f"{tmpdir}/tentative-source/conversation.txt"
+            expected_file_paths = [expected_transcript_path]
+
+            printer_job_enqueued_emissions = QSignalSpy(self.action.printer_job_enqueued)
+            self.assertTrue(printer_job_enqueued_emissions.isValid())
+            # Accept the confirmation dialog from a separate thread.
+            QTimer.singleShot(10, self._confirmation_dialog.accept)
+
+            self.action.trigger()
+
+            printer_job_enqueued_emissions.wait(MAX_SIGNAL_WAIT_TIME)  # milliseconds
+            self.assertEqual(1, len(printer_job_enqueued_emissions))
+            self.assertEqual([expected_file_paths], printer_job_enqueued_emissions[0])
+
+    def test_does_not_enqueue_any_printing_job_when_confirmation_dialog_rejected(self):
+        with TemporaryDirectory() as tmpdir:
+            self._controller.data_dir = tmpdir
+
+            printer_job_enqueued_emissions = QSignalSpy(self.action.printer_job_enqueued)
+            self.assertTrue(printer_job_enqueued_emissions.isValid())
+            # Reject the confirmation dialog from a separate thread.
+            QTimer.singleShot(10, self._confirmation_dialog.reject)
+
+            self.action.trigger()
+
+            printer_job_enqueued_emissions.wait(MAX_SIGNAL_WAIT_TIME)  # will time out
+            self.assertEqual(0, len(printer_job_enqueued_emissions))
+
+    @unittest.skip("work in progress")
+    def test_displays_error_dialog_when_printing_job_fails_after_confirmation_dialog_accepted(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_does_not_display_error_dialog_when_printing_job_fails_before_confirmation_dialog_accepted(  # noqa: E501
+        self,
+    ):
+        pass
