ec7e8dc7d895a922a91792b7ef3da0cd93df3faf
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2023-01-02 13:32:48 +1100

    Fix i18n date... happy new year!

--- securedrop_client/state/__init__.py
+++ securedrop_client/state/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright © 2022 The Freedom of the Press Foundation.
+# Copyright © 2022‒2023 The Freedom of the Press Foundation.
 """
 The internal state of the SecureDrop Client.
 """
