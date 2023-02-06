88c96e2bdf8b4cec41c460a653dca98d41088bfa
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-02-06 14:45:49 +0000

    update wording

--- securedrop_client/gui/actions.py
+++ securedrop_client/gui/actions.py
@@ -117,7 +117,7 @@ class DeleteSourcesAction(QAction):
     ) -> None:
         self.controller = controller
         self._confirmation_dialog = confirmation_dialog
-        text = _("Delete Multiple Source Accounts")
+        text = _("Delete Source Accounts")
 
         super().__init__(text, parent=parent)
         self.triggered.connect(self.trigger)
@@ -125,7 +125,6 @@ class DeleteSourcesAction(QAction):
 
     def trigger(self) -> None:
         sources = list(self.controller.checked_sources)
-        print(sources)
 
         if self.controller.api is None:
             self.controller.on_action_requiring_login()
