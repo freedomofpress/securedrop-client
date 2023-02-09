adfec1f0e42ef1bb7ea2185899d2768fcf734419
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-02-09 11:44:09 +0000

    securedrop-client 102.8.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for name in os.listdir("./securedrop_client/resources/images/"):
 
 setuptools.setup(
     name="securedrop-client",
-    version="101.9.0",
+    version="102.9.0",
     author="Freedom of the Press Foundation",
     author_email="securedrop@freedom.press",
     description="SecureDrop Workstation client application",
