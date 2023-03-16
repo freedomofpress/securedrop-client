3817abab33621d78c744474fd43aaa3dc322bbe4
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2023-03-16 16:33:54 +1100

    Fix improper coupling between transcript encoding and current locale (2)
    (cherry picked from commit b081519ab51a8569b848ed99beb1387a5f90e646)

--- tests/gui/test_actions.py
+++ tests/gui/test_actions.py
@@ -14,6 +14,7 @@ from securedrop_client.gui.actions import (
     DeleteConversationAction,
     DeleteSourceAction,
     DownloadConversation,
+    ExportConversationTranscriptAction,
     PrintConversationAction,
 )
 from securedrop_client.logic import Controller
@@ -268,6 +269,34 @@ class TestPrintConversationAction(unittest.TestCase):
                 controller = MagicMock(Controller, api=True, data_dir=tmp_dir)
                 source = MagicMock(Source, journalist_filename="mysterious-writer")
                 action = PrintConversationAction(menu, controller, source)
+                with patch("securedrop_client.gui.actions.ConversationTranscript") as transcript:
+                    transcript.return_value.__str__ = Mock(
+                        return_value="☠ A string with unicode characters."
+                    )
+
+                    action._export_device.run_printer_preflight_checks = (
+                        lambda: action._export_device.print_preflight_check_succeeded.emit()
+                    )
+                    action._export_device.print_transcript = (
+                        lambda transcript: action._export_device.print_succeeded.emit()
+                    )
+
+                    action.trigger()
+
+                    assert True  # the transcript is written without errors
+
+
+class TestExportConversationTranscriptAction(unittest.TestCase):
+    @patch("securedrop_client.gui.actions.ExportConversationTranscriptDialog")
+    def test_trigger(self, _):
+        with managed_locale():
+            locale.setlocale(locale.LC_ALL, ("en_US", "latin-1"))
+
+            with TemporaryDirectory() as tmp_dir:
+                menu = QMenu()
+                controller = MagicMock(Controller, api=True, data_dir=tmp_dir)
+                source = MagicMock(Source, journalist_filename="mysterious-writer")
+                action = ExportConversationTranscriptAction(menu, controller, source)
                 with patch("securedrop_client.gui.actions.ConversationTranscript") as transcript:
                     transcript.return_value.__str__ = Mock(
                         return_value="☠ A string with unicode characters."
