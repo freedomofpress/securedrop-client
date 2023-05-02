c54bef771290788f39617ef459aae3c35cf6ea6c
Cory Francis Myers <cory@freedom.press>
2023-05-02 16:17:45 -0700

    refactor: merge DraftReply model into Reply

---
+++ alembic/versions/dcba730b8834_merge_draftreply_into_reply.py
@@ -0,0 +1,56 @@
+"""merge DraftReply into Reply
+
+Revision ID: dcba730b8834
+Revises: b646a89cee64
+Create Date: 2023-05-03 15:47:22.424886
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "dcba730b8834"
+down_revision = "b646a89cee64"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_table("replysendstatuses")
+    op.drop_table("draftreplies")
+
+
+def downgrade():
+    op.create_table(
+        "draftreplies",
+        sa.Column("id", sa.INTEGER(), nullable=False),
+        sa.Column("uuid", sa.VARCHAR(length=36), nullable=False),
+        sa.Column("timestamp", sa.DATETIME(), nullable=False),
+        sa.Column("source_id", sa.INTEGER(), nullable=False),
+        sa.Column("journalist_id", sa.INTEGER(), nullable=True),
+        sa.Column("file_counter", sa.INTEGER(), nullable=False),
+        sa.Column("content", sa.TEXT(), nullable=True),
+        sa.Column("send_status_id", sa.INTEGER(), nullable=True),
+        sa.Column("sending_pid", sa.INTEGER(), nullable=True),
+        sa.ForeignKeyConstraint(
+            ["journalist_id"], ["users.id"], name="fk_draftreplies_journalist_id_users"
+        ),
+        sa.ForeignKeyConstraint(
+            ["send_status_id"],
+            ["replysendstatuses.id"],
+            name="fk_draftreplies_send_status_id_replysendstatuses",
+        ),
+        sa.ForeignKeyConstraint(
+            ["source_id"], ["sources.id"], name="fk_draftreplies_source_id_sources"
+        ),
+        sa.PrimaryKeyConstraint("id", name="pk_draftreplies"),
+        sa.UniqueConstraint("uuid", name="uq_draftreplies_uuid"),
+    )
+    op.create_table(
+        "replysendstatuses",
+        sa.Column("id", sa.INTEGER(), nullable=False),
+        sa.Column("name", sa.VARCHAR(length=36), nullable=False),
+        sa.PrimaryKeyConstraint("id", name="pk_replysendstatuses"),
+        sa.UniqueConstraint("name", name="uq_replysendstatuses_name"),
+    )
