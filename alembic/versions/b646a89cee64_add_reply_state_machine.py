8b609a087693a505c7b8b825f07272ae749d1822
Cory Francis Myers <cory@freedom.press>
2023-05-03 15:57:46 -0700

    feat(add_reply_state_machine): upgrade/downgrade data between Reply.state and legacy state columns

--- alembic/versions/b646a89cee64_add_reply_state_machine.py
+++ alembic/versions/b646a89cee64_add_reply_state_machine.py
@@ -18,6 +18,9 @@ depends_on = None
 
 def upgrade():
     """
+    Add Reply.state, then derive its values from the legacy state columns in
+    batch mode before dropping them.
+
     We have to do the non-addition operations in batch mode because SQLite
     doesn't support column-level ALTER or DROP statements.[1]
 
@@ -26,15 +29,32 @@ def upgrade():
     op.add_column(
         "replies", sa.Column("state", sa.String(length=100), nullable=False, server_default="Ready")
     )
+    op.execute("UPDATE replies SET state = 'Ready' WHERE is_decrypted IS 1")
+    op.execute("UPDATE replies SET state = 'DecryptionFailed' WHERE is_decrypted IS 0")
+    op.execute("UPDATE replies SET state = 'DownloadFailed' WHERE is_downloaded IS 0")
 
     with op.batch_alter_table("replies") as batch_op:
+
         batch_op.drop_column("is_downloaded")
         batch_op.drop_column("is_decrypted")
 
 
 def downgrade():
+    """
+    Restore the legacy state columns, then derive their values from Reply.state
+    before dropping it.
+
+    See above re: batch mode.
+    """
     op.add_column("replies", sa.Column("is_decrypted", sa.BOOLEAN(), nullable=True))
     op.add_column("replies", sa.Column("is_downloaded", sa.BOOLEAN(), nullable=True))
+    op.execute("UPDATE replies SET is_downloaded = 1 AND is_decrypted = 1 WHERE state = 'Ready'")
+    op.execute(
+        "UPDATE replies SET is_downloaded = 1 AND is_decrypted = 0 WHERE state = 'DecryptionFailed'"
+    )
+    op.execute(
+        "UPDATE replies SET is_downloaded = 0 AND is_decrypted = 0 WHERE state = 'DonloadFailed'"
+    )
 
     with op.batch_alter_table("replies") as batch_op:
         batch_op.drop_column("state")
