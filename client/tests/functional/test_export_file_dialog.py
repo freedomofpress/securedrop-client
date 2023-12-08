719cfce9fff62299eb17ce4cfb6310ade6c6776b
Kunal Mehta <legoktm@debian.org>
2023-12-08 11:49:35 -0500

    Properly mock ExportService functions
    It now needs to emit the correct values rather than just returning
    statuses.

--- client/tests/functional/test_export_file_dialog.py
+++ client/tests/functional/test_export_file_dialog.py
@@ -5,7 +5,6 @@ The tests are based upon the client testing descriptions here:
 https://github.com/freedomofpress/securedrop-client/wiki/Test-plan#basic-client-testing
 """
 import pytest
-from flaky import flaky
 from PyQt5.QtCore import Qt
 
 from securedrop_client.gui.widgets import FileWidget, SourceConversationWrapper
@@ -22,21 +21,18 @@ def _setup_export(functional_test_logged_in_context, qtbot, mocker, mock_export_
     """
     Helper. Set up export test context and return reference to export dialog.
     """
-    mocker.patch(
-        "securedrop_client.export.getService", return_value=mock_export_service
-    )
+    mocker.patch("securedrop_client.export.getService", return_value=mock_export_service)
 
     gui, controller = functional_test_logged_in_context
 
     def check_for_sources():
-        assert len(list(gui.main_view.source_list.source_items.keys()))
+        assert list(gui.main_view.source_list.source_items) != []
 
     # Select the first source in the source list
     qtbot.waitUntil(check_for_sources, timeout=TIME_RENDER_SOURCE_LIST)
 
     # Select the second source in the list to avoid marking unseen sources as seen
-    source_id = list(gui.main_view.source_list.source_items.keys())[1]
-    source_item = gui.main_view.source_list.source_items[source_id]
+    source_item = list(gui.main_view.source_list.source_items.values())[1]
     source_widget = gui.main_view.source_list.itemWidget(source_item)
     qtbot.mouseClick(source_widget, Qt.LeftButton)
     qtbot.wait(TIME_CLICK_ACTION)
@@ -45,15 +41,13 @@ def _setup_export(functional_test_logged_in_context, qtbot, mocker, mock_export_
         assert gui.main_view.view_layout.itemAt(0)
         conversation = gui.main_view.view_layout.itemAt(0).widget()
         assert isinstance(conversation, SourceConversationWrapper)
-        file_id = list(conversation.conversation_view.current_messages.keys())[2]
-        file_widget = conversation.conversation_view.current_messages[file_id]
+        file_widget = list(conversation.conversation_view.current_messages.values())[2]
         assert isinstance(file_widget, FileWidget)
 
     # Get the selected source conversation that contains a file attachment
     qtbot.waitUntil(conversation_with_file_is_rendered, timeout=TIME_RENDER_CONV_VIEW)
     conversation = gui.main_view.view_layout.itemAt(0).widget()
-    file_id = list(conversation.conversation_view.current_messages.keys())[2]
-    file_widget = conversation.conversation_view.current_messages[file_id]
+    file_widget = list(conversation.conversation_view.current_messages.values())[2]
 
     # If the file is not downloaded, click on the download button
     if file_widget.download_button.isVisible():
@@ -64,6 +58,7 @@ def _setup_export(functional_test_logged_in_context, qtbot, mocker, mock_export_
     def check_for_export_dialog():
         assert file_widget.export_dialog
 
+    assert file_widget.export_button.isVisible()
     # Begin exporting the file
     qtbot.mouseClick(file_widget.export_button, Qt.LeftButton)
     qtbot.wait(TIME_CLICK_ACTION)
@@ -72,60 +67,19 @@ def _setup_export(functional_test_logged_in_context, qtbot, mocker, mock_export_
     return file_widget.export_dialog
 
 
-@flaky
 @pytest.mark.vcr()
-def test_export_file_dialog(functional_test_logged_in_context, qtbot, mocker, mock_export_service):
+def test_export_file_dialog_locked(
+    functional_test_logged_in_context, qtbot, mocker, mock_export_service
+):
     """
     Download a file, export it, and verify that the export is complete by checking that the label of
     the export dialog's continue button is "DONE".
     """
-    mocker.patch(
-        "securedrop_client.export.getService", return_value=mock_export_service
+    export_dialog = _setup_export(
+        functional_test_logged_in_context, qtbot, mocker, mock_export_service
     )
 
-    gui, controller = functional_test_logged_in_context
-
-    def check_for_sources():
-        assert len(list(gui.main_view.source_list.source_items.keys()))
-
-    # Select the first source in the source list
-    qtbot.waitUntil(check_for_sources, timeout=TIME_RENDER_SOURCE_LIST)
-
-    # Select the second source in the list to avoid marking unseen sources as seen
-    source_id = list(gui.main_view.source_list.source_items.keys())[1]
-    source_item = gui.main_view.source_list.source_items[source_id]
-    source_widget = gui.main_view.source_list.itemWidget(source_item)
-    qtbot.mouseClick(source_widget, Qt.LeftButton)
-    qtbot.wait(TIME_CLICK_ACTION)
-
-    def conversation_with_file_is_rendered():
-        assert gui.main_view.view_layout.itemAt(0)
-        conversation = gui.main_view.view_layout.itemAt(0).widget()
-        assert isinstance(conversation, SourceConversationWrapper)
-        file_id = list(conversation.conversation_view.current_messages.keys())[2]
-        file_widget = conversation.conversation_view.current_messages[file_id]
-        assert isinstance(file_widget, FileWidget)
-
-    # Get the selected source conversation that contains a file attachment
-    qtbot.waitUntil(conversation_with_file_is_rendered, timeout=TIME_RENDER_CONV_VIEW)
-    conversation = gui.main_view.view_layout.itemAt(0).widget()
-    file_id = list(conversation.conversation_view.current_messages.keys())[2]
-    file_widget = conversation.conversation_view.current_messages[file_id]
-
-    # If the file is not downloaded, click on the download button
-    if file_widget.download_button.isVisible():
-        qtbot.mouseClick(file_widget.download_button, Qt.LeftButton)
-        qtbot.wait(TIME_CLICK_ACTION)
-        qtbot.wait(TIME_FILE_DOWNLOAD)
-
-    def check_for_export_dialog():
-        assert file_widget.export_dialog
-
-    # Begin exporting the file
-    qtbot.mouseClick(file_widget.export_button, Qt.LeftButton)
-    qtbot.wait(TIME_CLICK_ACTION)
-    qtbot.waitUntil(check_for_export_dialog, timeout=TIME_RENDER_EXPORT_DIALOG)
-    export_dialog = file_widget.export_dialog
+    assert export_dialog.passphrase_form.isHidden() is True
 
     def check_password_form():
         assert export_dialog.passphrase_form.isHidden() is False
@@ -146,7 +100,6 @@ def test_export_file_dialog(functional_test_logged_in_context, qtbot, mocker, mo
     assert export_dialog.continue_button.text() == "DONE"
 
 
-@flaky
 @pytest.mark.vcr()
 def test_export_file_dialog_device_already_unlocked(
     functional_test_logged_in_context, qtbot, mocker, mock_export_service_unlocked_device
