cc76caf6eae5de03d97a804c5710eaf1fe1750ca
Kunal Mehta <legoktm@debian.org>
2024-02-20 15:15:15 -0500

    Remove Python implementation of proxy
    Now replaced by the Rust version.

--- proxy/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/user/projects/securedrop-proxy
-virtualenv .venv
-source .venv/bin/activate
-pip install --require-hashes -r requirements.txt
-pip install --require-hashes -r dev-requirements.txt
-./sd-proxy.py ./config.yaml
