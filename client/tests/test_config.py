9a2e3b5efa95d44e4680d873affa2949d0481a55
Cory Francis Myers <cory@freedom.press>
2024-03-07 12:31:12 -0800

    chore: remove unused Config.is_valid(); just test Config.load()
    thread: https://github.com/freedomofpress/securedrop-client/pull/1883/files#r1508506960

--- client/tests/test_config.py
+++ client/tests/test_config.py
@@ -4,12 +4,9 @@ from unittest.mock import patch
 from securedrop_client.config import Config
 
 
-@patch.dict(os.environ, {"SD_SUBMISSION_KEY_FPR": ""})
-def test_missing_journalist_key_fpr():
-    """
-    If a key is missing, the config can still be loaded, but is "invalid".
-    """
+@patch.dict(os.environ, {"SD_SUBMISSION_KEY_FPR": "foobar"})
+def test_config():
     config = Config.load()
 
-    assert config.journalist_key_fingerprint is None
-    assert config.is_valid is False
+    assert config.journalist_key_fingerprint == "foobar"
+    assert config.gpg_domain is None
