8c477b74f0c74f5d2e18bca7d733a221e643dc0b
Cory Francis Myers <cory@freedom.press>
2024-03-11 15:42:58 -0700

    docs(read): safety

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -27,6 +27,8 @@ fn read(name: &str) -> Result<String> {
 
     let path_raw = CString::new(path.clone()).unwrap().into_raw();
     let mut len: u32 = 0;
+    // SAFETY: path_raw will owned by the C caller and MUST be retaken.  When
+    // len == 0, value_unsafe = NUL and MUST NOT be passed to CStr::from_ptr().
     let value_raw = unsafe {
         let db = qdb_open(ptr::null_mut());
         let value_unsafe = qdb_read(db, path_raw, &mut len);
