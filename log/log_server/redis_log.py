90424f8fe8f00811426461ea12516be101eb5c7e
deeplow <francisco@freedom.press>
2024-05-09 06:40:40 -0400

    log: reorganize code to split server from client
    Securedrop-log had a lot of different files belonging to two
    distinct architectural components: a server and a client with
    no actual coupling. This change will help accommodate the creation
    of separate systemd services for the client and server.
    
    Refs securedrop-workstation#1004
    
    NOTE: securedrop-log-saver and securedrop-redis-log now have python
    extensions and their binary form is created through setup.py's
    console scripts.

---
+++ log/log_server/redis_log.py
@@ -0,0 +1,40 @@
+#!/opt/venvs/securedrop-log/bin/python3
+
+
+import sys
+
+import redis
+
+
+def sanitize_line(untrusted_line):
+    line = bytearray(untrusted_line)
+    for i, c in enumerate(line):
+        if c >= 0x20 and c <= 0x7E:
+            pass
+        else:
+            line[i] = 0x2E
+    return bytearray(line).decode("ascii")
+
+
+def log(rd, msg, vmname="remote"):
+    redis_msg = f"{vmname}::{msg}"
+    rd.rpush("syslogmsg", redis_msg)
+
+
+def main():
+    stdin = sys.stdin.buffer  # python3
+    rd = redis.Redis()
+
+    # the first line is always the remote vm name
+    untrusted_line = stdin.readline()
+    qrexec_remote = untrusted_line.rstrip(b"\n").decode("utf-8")
+    while True:
+        untrusted_line = stdin.readline()
+        if untrusted_line == b"":
+            break
+
+        log(rd, sanitize_line(untrusted_line.rstrip(b"\n")), qrexec_remote)
+
+
+if __name__ == "__main__":
+    main()
