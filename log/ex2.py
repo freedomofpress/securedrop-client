588988a9ba944b94fc47d50b6c9e1183d4051ea7
Kunal Mehta <legoktm@debian.org>
2024-05-17 19:03:26 -0400

    Remove unused securedrop-log code
    If you look at the first commit to securedrop-log (9ff16b6), the
    original idea was that clients directly log to the VM via a custom log
    handler, but that was dropped later on in favor of rsyslog (see
    <https://github.com/freedomofpress/securedrop-log/issues/7> and
    29c1cf7), but the old approach was never deleted.
    
    This removes that old approach was well as example code.
    
    Fixes #1998.

--- log/ex2.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-class Hello:
-    def __init__(self, *args, **kwargs):
-        self.logger = logging.getLogger(__name__)
-
-    def talk(self, msg):
-        self.logger.debug(msg)
