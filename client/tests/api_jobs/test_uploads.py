0d521f5ce78f662f18da73849c18ab03de1cd1c9
Cory Francis Myers <cory@freedom.press>
2025-01-29 18:26:20 -0800

    fix(SendReplyJob): validate server-provided filename
    This is a compromise: We have to at least consult the server-provided
    filename no matter what, because that's how we reconcile our local
    ordering with the server's authoritative ordering within the source
    conversation.  At that point, we might as well just use it, subject to
    the same validation enforced for metadata syncs in
    storage.sanitize_submissions_or_replies().
    
    (cherry picked from commit 6014eece28f04092d7bcf32ebd620e9f394f07f9)

--- client/tests/api_jobs/test_uploads.py
+++ client/tests/api_jobs/test_uploads.py
@@ -41,7 +41,7 @@ def test_send_reply_success(homedir, mocker, session, session_maker, reply_statu
     mock_encrypt = mocker.patch.object(gpg, "encrypt_to_source", return_value=encrypted_reply)
     msg = "wat"
 
-    mock_reply_response = sdk.Reply(uuid=msg_uuid, filename="5-dummy-reply")
+    mock_reply_response = sdk.Reply(uuid=msg_uuid, filename="5-dummy-reply.gpg")
     api_client.reply_source = mocker.MagicMock()
     api_client.reply_source.return_value = mock_reply_response
 
@@ -147,7 +147,7 @@ def test_drafts_ordering(homedir, mocker, session, session_maker, reply_status_c
     mock_encrypt = mocker.patch.object(gpg, "encrypt_to_source", return_value=encrypted_reply)
     msg = "wat"
 
-    mock_reply_response = sdk.Reply(uuid=msg_uuid, filename="2-dummy-reply")
+    mock_reply_response = sdk.Reply(uuid=msg_uuid, filename="2-dummy-reply.gpg")
     api_client.reply_source = mocker.MagicMock()
     api_client.reply_source.return_value = mock_reply_response
 
