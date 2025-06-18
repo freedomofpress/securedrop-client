542ee210f589184329e877f67ba6343d5cd17412
Micah Lee <micah@micahflee.com>
2025-06-18 16:27:27 -0700

    Add type definitions for better-sqlite3, and fix lint

--- app/src/database.ts
+++ app/src/database.ts
@@ -1,23 +1,23 @@
-import path from 'path';
-import { app } from 'electron';
-import Database from 'better-sqlite3';
+import path from "path";
+import { app } from "electron";
+import Database from "better-sqlite3";
 
 let db: Database.Database | null = null;
 
 export const openDatabase = () => {
-    if (db) {
-        return db;
-    }
-
-    const dbPath = path.join(app.getPath('userData'), 'db.sqlite');
-    db = new Database(dbPath, {});
-    db.pragma('journal_mode = WAL');
+  if (db) {
     return db;
-}
+  }
+
+  const dbPath = path.join(app.getPath("userData"), "db.sqlite");
+  db = new Database(dbPath, {});
+  db.pragma("journal_mode = WAL");
+  return db;
+};
 
 export const closeDatabase = () => {
-    if (db) {
-        db.close();
-        db = null;
-    }
-}
+  if (db) {
+    db.close();
+    db = null;
+  }
+};
