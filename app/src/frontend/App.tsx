84c9a22901090a628dd11e7a3f7dee1a32bf989b
vicki <vicki@freedom.press>
2025-07-08 15:12:38 -0400

    add ant design + tailwind css

--- app/src/frontend/App.tsx
+++ app/src/frontend/App.tsx
@@ -1,3 +1,5 @@
+import { Button } from "antd";
+
 function App() {
   const dummyRequest = async function () {
     console.log("sending dummy request");
@@ -13,10 +15,25 @@ function App() {
 
   return (
     <div>
-      <p>Hello world</p>
-      <button onClick={() => dummyRequest()} title="Dummy Request">
-        Dummy Request
-      </button>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Hello world!</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <Button
+            type="primary"
+            onClick={() => dummyRequest()}
+            title="Dummy Request"
+          >
+            Dummy Request
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
