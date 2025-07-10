42ec66b3a3feb75e9f05849fb78fee1a4305a37f
Micah Lee <micah@micahflee.com>
2025-07-10 11:37:44 -0700

    Automatically run migrations when starting the app

---
+++ app/src/main.ts
@@ -0,0 +1,49 @@
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from "node:path";
+
+import { openDatabase, closeDatabase, runMigrations } from "./database";
+import { proxy, ProxyRequest } from "./proxy";
+import { run } from "node:test";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const db = openDatabase();
+runMigrations();
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+    );
+  }
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+app.on("before-quit", () => {
+  closeDatabase();
+});
+
+ipcMain.handle("request", async (event, request: ProxyRequest) => {
+  const result = await proxy(request);
+  return result;
+});
