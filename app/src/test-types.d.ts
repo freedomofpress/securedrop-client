cfa53507148c1ab304b05ab0b82b6307594e2a4c
Micah Lee <micah@micahflee.com>
2025-07-11 11:48:30 -0700

    Fix lint

--- app/src/test-types.d.ts
+++ app/src/test-types.d.ts
@@ -4,10 +4,10 @@
 import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 
 declare module "vitest" {
-  interface Assertion<T = any>
+  interface Assertion<T = unknown>
     extends jest.Matchers<void>,
       TestingLibraryMatchers<T, void> {}
   interface AsymmetricMatchersContaining
     extends jest.Matchers<void>,
-      TestingLibraryMatchers<any, void> {}
+      TestingLibraryMatchers<unknown, void> {}
 }
