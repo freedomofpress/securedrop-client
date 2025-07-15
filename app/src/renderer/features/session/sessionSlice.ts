6275c6846347e2f1c947aec3ea5e5b259d0b4107
Micah Lee <micah@micahflee.com>
2025-07-15 13:57:26 -0700

    Remove comment from sessionSlice

--- app/src/renderer/features/session/sessionSlice.ts
+++ app/src/renderer/features/session/sessionSlice.ts
@@ -1,10 +1,3 @@
-/*
-WARNING: This is not a real session!  It does not, and should not, provide a
-real session that can be used for subsequent requests; use real session storage
-for that.  It merely caches for demonstration purposes some information returned
-by a successful authentication request.
-*/
-
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../../store";
