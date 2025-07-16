06dfc870c8b4e27754ed190466bed6acaa570b81
Micah Lee <micah@micahflee.com>
2025-07-16 09:57:00 -0700

    Update test to check for title and version

--- app/tests/renderer/views/SignIn.test.tsx
+++ app/tests/renderer/views/SignIn.test.tsx
@@ -5,12 +5,14 @@ import SignInView from "../../../src/renderer/views/SignIn";
 import { renderWithProviders } from "../../../src/renderer/test-component-setup";
 
 describe("SignInView Component", () => {
-  it('says the string "Sign in to SecureDrop"', async () => {
+  it("says title and version", async () => {
     renderWithProviders(<SignInView />);
 
     // Wait for the async useEffect to complete
     await waitFor(() => {
       expect(screen.getByText("Sign in to SecureDrop")).toBeInTheDocument();
+      // getVersion is mocked to return "1.0.0"
+      expect(screen.getByText("SecureDrop App v1.0.0")).toBeInTheDocument();
     });
   });
 });
