0057372f38c34e9c0df9f25788a8d4b7f83aa5cf
vicki <vicki@freedom.press>
2025-07-16 11:14:19 -0400

    lint

--- app/integration_tests/setup.ts
+++ app/integration_tests/setup.ts
@@ -1,4 +1,4 @@
-import { exec, execSync } from "child_process";
+import { execSync } from "child_process";
 import { beforeAll, afterAll } from "vitest";
 import { GenericContainer, StartedTestContainer } from "testcontainers";
 
