3a8def7f1a07487d50326353d9c72ba4815b20ff
Micah Lee <micah@micahflee.com>
2025-07-21 15:59:16 -0700

    Gate routes that require authentication

--- app/src/renderer/App.tsx
+++ app/src/renderer/App.tsx
@@ -1,34 +1,44 @@
-import { Routes, Route, useNavigate } from "react-router";
-import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router";
 
 import InboxView from "./views/Inbox";
 import SignInView from "./views/SignIn";
-import { useAppDispatch, useAppSelector } from "./hooks";
-import { setUnauth, SessionStatus } from "./features/session/sessionSlice";
+import { useAppSelector } from "./hooks";
+import { SessionStatus } from "./features/session/sessionSlice";
 
 function App() {
-  const session = useAppSelector((state) => state.session);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  // If we're not in offline mode and there's no session, redirect to sign-in
-  useEffect(() => {
-    if (
-      session.status !== SessionStatus.Offline &&
-      !session.authData?.journalistUUID
-    ) {
-      console.log("No session found, redirecting to sign-in");
-      dispatch(setUnauth());
-      navigate("/sign-in");
-    }
-  }, [session.status, session.authData?.journalistUUID, dispatch, navigate]);
-
   return (
     <Routes>
-      <Route path="/" element={<InboxView />} />
+      <Route
+        index
+        path="/"
+        element={
+          <ProtectedRoute>
+            <InboxView />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/sign-in" element={<SignInView />} />
     </Routes>
   );
 }
+
+// Gate the routes that require authentication
+
+type ProtectedRouteProps = {
+  children: React.ReactNode;
+};
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const session = useAppSelector((state) => state.session);
+
+  if (
+    session.status !== SessionStatus.Auth &&
+    session.status !== SessionStatus.Offline
+  ) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  return children;
+};
 
 export default App;
