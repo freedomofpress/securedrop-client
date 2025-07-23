1df0799ebecd4d1f27982217da8b91b5c1bc3430
Micah Lee <micah@micahflee.com>
2025-07-23 10:52:23 -0700

    Expand database schema; add base query functions
    Add getSources(), getItems() and getSourceWithItems() functions
    that will later be added to IPC for the frontend to fetch data
    from the database.
    
    For performance, we've turned some of the JSON metadata into
    separate generated columns that can be indexed and queried.
    
    Co-authored-by: Kunal Mehta <legoktm@debian.org>

--- app/src/main/database/migrations/20250710180544_create_initial_schema.sql
+++ app/src/main/database/migrations/20250710180544_create_initial_schema.sql
@@ -1,16 +1,37 @@
 -- migrate:up
-create table sources (
-  uuid text primary key,
-  data json
-);
+create table
+  sources (
+    uuid text primary key,
+    data json,
+    is_seen integer generated always as (json_extract (data, '$.is_seen')) stored,
+    has_attachment integer generated always as (json_extract (data, '$.has_attachment')) stored,
+    show_message_preview integer default 0,
+    message_preview text
+  );
 
-create table items (
-  uuid text primary key,
-  data json,
-  plaintext text,
-  filename text
-);
+create table
+  items (
+    uuid text primary key,
+    data json,
+    plaintext text,
+    filename text,
+    source_uuid text generated always as (json_extract (data, '$.source')) stored,
+    kind text generated always as (json_extract (data, '$.kind')) stored,
+    is_read integer generated always as (json_extract (data, '$.is_read')) stored,
+    last_updated integer generated always as (json_extract (data, '$.last_updated')) stored
+  );
+
+create index idx_items_source_uuid on items (source_uuid);
+
+create index idx_items_kind on items (kind);
+
+create index idx_items_is_read on items (is_read);
+
+create index idx_items_last_updated on items (last_updated);
+
+create index idx_sources_uuid on sources (uuid);
 
 -- migrate:down
 drop table if exists items;
+
 drop table if exists sources;
