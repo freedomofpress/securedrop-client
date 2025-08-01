88ccce27db94e5b1b2244d7df9cd1c5ce85dc674
Micah Lee <micah@micahflee.com>
2025-08-01 14:04:35 -0700

    Make conversation list have a scrollbar, instead of the full window

--- app/src/renderer/views/Inbox/MainContent/Items.tsx
+++ app/src/renderer/views/Inbox/MainContent/Items.tsx
@@ -17,13 +17,15 @@ function Items({ sourceWithItems }: ItemsProps) {
   const designation = sourceWithItems.data.journalistDesignation;
 
   return (
-    <div className="flex flex-col h-full w-full p-4">
-      <div className="flex-1 overflow-y-auto pr-1">
-        {sourceWithItems.items.map((item) => (
-          <Item key={item.uuid} item={item} designation={designation} />
-        ))}
+    <div className="flex flex-col h-full w-full min-h-0">
+      <div className="flex-1 min-h-0 relative">
+        <div className="absolute inset-0 overflow-y-auto p-4 pb-0">
+          {sourceWithItems.items.map((item) => (
+            <Item key={item.uuid} item={item} designation={designation} />
+          ))}
+        </div>
       </div>
-      <div className="pt-4">
+      <div className="flex-shrink-0 p-4 pt-0">
         <Form layout="vertical">
           <Form.Item style={{ marginBottom: 0 }}>
             <div className="relative">
