da934e94687cd9e6e660642e757f59680af36c58
vicki <vicki@freedom.press>
2025-10-09 13:54:47 -0400

    trigger sync in background, only show loading when no other state exists

--- app/src/renderer/views/Inbox.tsx
+++ app/src/renderer/views/Inbox.tsx
@@ -2,11 +2,19 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import type { AppDispatch } from "../store";
 import { fetchJournalists } from "../features/journalists/journalistsSlice";
+import { syncMetadata } from "../features/sync/syncSlice";
+import { useAppSelector } from "../hooks";
 import Sidebar from "./Inbox/Sidebar";
 import MainContent from "./Inbox/MainContent";
 
 function InboxView() {
   const dispatch = useDispatch<AppDispatch>();
+  const session = useAppSelector((state) => state.session);
+
+  // Trigger sync in the background every second
+  setInterval(() => {
+    dispatch(syncMetadata(session.authData?.token));
+  }, 1000);
 
   useEffect(() => {
     dispatch(fetchJournalists());
