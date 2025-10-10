95f71436ac974c0e9d02feef19750fd51f0ea58b
vicki <vicki@freedom.press>
2025-10-10 16:14:53 -0400

    include schema.sql changes

--- app/src/main/database/schema.sql
+++ app/src/main/database/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "schema_migrations" (version varchar(128) primary key
 CREATE TABLE sources (
   uuid text primary key,
   data json
-, version text, is_seen integer generated always as (json_extract (data, '$.is_seen')) stored, has_attachment integer generated always as (json_extract (data, '$.has_attachment')) stored, show_message_preview integer default 0, message_preview text, is_starred text generated always as (json_extract (data, '$.is_starred')));
+, version text, is_seen integer generated always as (json_extract (data, '$.is_seen')) stored, has_attachment integer generated always as (json_extract (data, '$.has_attachment')) stored, show_message_preview integer default 0, message_preview text);
 CREATE TABLE items (
   uuid text primary key,
   data json,
@@ -32,21 +32,54 @@ CREATE TABLE journalists (
 );
 CREATE INDEX idx_items_fetch_status ON items (fetch_status);
 CREATE TABLE pending_events (
-    snowflake_id INTEGER PRIMARY KEY,
-
-    -- only one of source_uuid OR item_uuid is set
-    source_uuid INTEGER REFERENCES sources(uuid),
-    item_uuid INTEGER REFERENCES items(uuid),
-
-    type INTEGER NOT NULL,
-
-    -- additional event data
-    data json
-);
+        snowflake_id INTEGER PRIMARY KEY,
+        source_uuid INTEGER REFERENCES sources (uuid),
+        -- pending items may not exist in the items table, so
+        -- we don't add the fkey constraint
+        item_uuid INTEGER,
+        type INTEGER NOT NULL,
+        -- additional event data
+        data json,
+        -- only one of source_uuid OR item_uuid is set
+        CHECK (
+            NOT (
+                source_uuid IS NOT NULL
+                AND item_uuid IS NOT NULL
+            )
+        )
+    );
 CREATE VIEW sources_projected AS
+WITH
+    -- Select latest starred value from pending_events
+    latest_starred AS (
+        SELECT
+            source_uuid,
+            CASE
+                WHEN type = 5 THEN true -- Starred
+                WHEN type = 6 THEN false -- Unstarred
+            END AS starred_value
+        FROM
+            (
+                SELECT
+                    source_uuid,
+                    type
+                FROM
+                    pending_events
+                WHERE
+                    type in (5, 6) -- Starred or Unstarred
+                ORDER BY
+                    snowflake_id DESC
+                LIMIT
+                    1
+            )
+    )
 SELECT
     sources.uuid,
-    sources.data,
+    -- project Starred/Unstarred event
+    CASE
+        WHEN latest_starred.starred_value IS NOT NULL THEN json_set (sources.data, '$.is_starred', starred_value)
+        ELSE sources.data
+    END AS data,
     sources.version,
     sources.has_attachment,
     sources.show_message_preview,
@@ -54,43 +87,33 @@ SELECT
     -- project Seen event
     CASE
         WHEN EXISTS (
-            SELECT 1
-            FROM pending_events
-            WHERE pending_events.source_uuid = sources.uuid
-            -- type: Seen
-            AND pending_events.type = 7
-        )
-        THEN 1
-        ELSE sources.is_seen
-    END AS is_seen,
-    -- project Starred/Unstarred event
-    CASE
-        WHEN EXISTS (
-            SELECT 1
-            FROM pending_events
-            WHERE pending_events.source_uuid = sources.uuid
-            -- type: Starred
-            AND pending_events.type = 5
+            SELECT
+                1
+            FROM
+                pending_events
+            WHERE
+                pending_events.source_uuid = sources.uuid
+                -- type: Seen
+                AND pending_events.type = 7
         ) THEN 1
-        WHEN EXISTS (
-            SELECT 1
-            FROM pending_events
-            WHERE pending_events.source_uuid = sources.uuid
-            -- type: Unstarred
-            AND pending_events.type = 6
-        ) THEN 0
-        ELSE sources.is_starred
-    END AS is_starred
-FROM sources
--- project SourceDeleted event
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM pending_events
-    WHERE pending_events.source_uuid = sources.uuid
-    -- type: SourceDeleted
-    AND pending_events.type = 3
-)
-/* sources_projected(uuid,data,version,has_attachment,show_message_preview,message_preview,is_seen,is_starred) */;
+        ELSE sources.is_seen
+    END AS is_seen
+FROM
+    sources
+    LEFT JOIN latest_starred ON latest_starred.source_uuid = sources.uuid
+WHERE
+    -- project SourceDeleted event
+    NOT EXISTS (
+        SELECT
+            1
+        FROM
+            pending_events
+        WHERE
+            pending_events.source_uuid = sources.uuid
+            -- type: SourceDeleted
+            AND pending_events.type = 3
+    )
+/* sources_projected(uuid,data,version,has_attachment,show_message_preview,message_preview,is_seen) */;
 CREATE VIEW items_projected AS
 SELECT
     items.uuid,
@@ -106,42 +129,74 @@ SELECT
     items.fetch_status,
     items.fetch_last_updated_at,
     items.fetch_retry_attempts
-FROM items
--- project ReplyDeleted event
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM pending_events
-    WHERE pending_events.item_uuid = items.uuid
-    -- type: ReplyDeleted
-    AND pending_events.type = 2
-)
--- project SourceDeleted, SourceConversationDeleted event
-AND NOT EXISTS (
-    SELECT 1
-    FROM pending_events
-    WHERE pending_events.source_uuid = items.source_uuid
-    -- type: SourceDeleted, SourceConversationDeleted
-    AND pending_events.type IN (3, 4)
-)
--- project ReplySent event
+FROM
+    items
+    -- project ItemDeleted event
+WHERE
+    NOT EXISTS (
+        SELECT
+            1
+        FROM
+            pending_events
+        WHERE
+            pending_events.item_uuid = items.uuid
+            -- type: ItemDeleted
+            AND pending_events.type = 2
+    )
+    -- project SourceDeleted, SourceConversationDeleted event
+    AND NOT EXISTS (
+        SELECT
+            1
+        FROM
+            pending_events
+        WHERE
+            pending_events.source_uuid = items.source_uuid
+            -- type: SourceDeleted, SourceConversationDeleted
+            AND pending_events.type IN (3, 4)
+    )
+    -- project ReplySent event
 UNION ALL
 SELECT
-    pending_events.item_uuid AS uuid,
-    json_extract(pending_events.data, '$.metadata') as data,
+    json_extract (pending_events.data, '$.metadata.uuid') AS uuid,
+    json_extract (pending_events.data, '$.metadata') as data,
     NULL as version,
-    json_extract(pending_events.data, '$.text') AS plaintext,
+    json_extract (pending_events.data, '$.text') AS plaintext,
     NULL as filename,
     'reply' AS kind,
     NULL as is_read,
     NULL as last_updated,
-    pending_events.source_uuid AS source_uuid,
+    json_extract (pending_events.data, '$.metadata.source') AS source_uuid,
     NULL as fetch_progress,
     NULL as fetch_status,
     NULL as fetch_last_updated_at,
     NULL as fetch_retry_attempts
-FROM pending_events
--- type: ReplySent
-WHERE pending_events.type = 1
+FROM
+    pending_events
+WHERE
+    -- type: ReplySent
+    pending_events.type = 1
+    -- Check that there is no later overriding deletion
+    AND NOT EXISTS (
+        SELECT
+            1
+        FROM
+            pending_events later
+        WHERE
+            (
+                -- SourceDeleted or SourceConversationDeleted
+                (
+                    later.source_uuid = json_extract (pending_events.data, '$.metadata.source')
+                    AND later.type IN (3, 4)
+                )
+                OR
+                -- ItemDeleted
+                (
+                    later.item_uuid = json_extract (pending_events.data, '$.metadata.uuid')
+                    AND later.type = 2
+                )
+            )
+            AND later.snowflake_id > pending_events.snowflake_id
+    )
 /* items_projected(uuid,data,version,plaintext,filename,kind,is_read,last_updated,source_uuid,fetch_progress,fetch_status,fetch_last_updated_at,fetch_retry_attempts) */;
 -- Dbmate schema migrations
 INSERT INTO "schema_migrations" (version) VALUES
