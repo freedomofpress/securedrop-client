8f1866bcf2ca3c00554260d45545d743402b6f82
Kevin O'Gorman <kog@freedom.press>
2026-01-06 18:56:03 -0500

    Add initial Source dropdown menu

--- app/src/renderer/views/Inbox/MainContent/Header.tsx
+++ app/src/renderer/views/Inbox/MainContent/Header.tsx
@@ -3,6 +3,40 @@ import { useTranslation } from "react-i18next";
 import { toTitleCase, formatDateLong } from "../../../utils";
 import Avatar from "../../../components/Avatar";
 import type { SourceWithItems } from "../../../../types";
+import { MenuProps, Dropdown, Button, Flex } from "antd";
+import { EllipsisOutlined } from "@ant-design/icons";
+
+const handleMenuClick: MenuProps['onClick'] = (e) => {
+  switch (e.key) {
+    case "exportTranscript":
+      console.log("export transcript...tk");
+      break;
+
+    case "printTranscript":
+      console.log("print transcript...tk");
+      break;
+    default:
+      break;
+  }
+};
+
+
+const items: MenuProps['items'] = [
+  {
+    key: "exportTranscript",
+    label: "Export transcript...",
+  },
+  {
+    key: "printTranscript",
+    label: "Print transcript...",
+  },
+];
+
+const menuProps = {
+  items,
+  onClick: handleMenuClick,
+};
+
 
 interface HeaderProps {
   sourceUuid?: string;
@@ -36,18 +70,28 @@ const Header = memo(function Header({
   );
 
   return (
-    <>
-      <Avatar designation={designation} isActive={false} />
-      <div className="ml-2">
-        <p data-testid="conversation-header-designation">{designation}</p>
-        <p
-          className="text-sm text-gray-600"
-          data-testid="conversation-header-last-activity"
-        >
-          {t("lastSourceActivity")}: {formattedLastSeen}
-        </p>
-      </div>
-    </>
+    <Flex justify="space-between" align="center">
+      <Flex>
+        <Avatar designation={designation} isActive={false} />
+        <div className="ml-2">
+          <p data-testid="conversation-header-designation">{designation}</p>
+          <p
+            className="text-sm text-gray-600"
+            data-testid="conversation-header-last-activity"
+          >
+            {t("lastSourceActivity")}: {formattedLastSeen}
+          </p>
+        </div>
+      </Flex>
+      <Dropdown menu={menuProps}>
+          <Button
+            type="text"
+            icon={
+              <EllipsisOutlined style={{ color: "gray", fontSize: "24px" }} />
+            }
+          />
+      </Dropdown>
+    </Flex>
   );
 });
 
