f8fc607e8749a1f14543fcc3d2e4c5a2fdb888c8
Cory Francis Myers <cory@freedom.press>
2026-02-12 12:29:40 -0800

    refactor(getRealisticTimeout): factor out for use outside of queues

--- app/src/main/fetch/queue.ts
+++ app/src/main/fetch/queue.ts
@@ -6,6 +6,7 @@ import { Writable } from "stream";
 
 import { BufferedWriter } from "./bufferedWriter";
 import { DB } from "../database";
+import { getRealisticTimeout } from "../timeouts";
 import {
   AuthedRequest,
   FetchStatus,
@@ -69,34 +70,6 @@ export class TaskQueue {
 
   getAuthToken(): string {
     return this.authToken ? this.authToken : "";
-  }
-
-  // Calculate a realistic timeout in milliseconds based on the size of the download.
-  // This scales the timeout per file so that it increases as the file size increases.
-  //
-  // Based on Tor metrics, reasonable estimations for download times over Tor:
-  //   50 KiB  (51200 bytes)   =  6   seconds  (8,533 bytes/second)
-  //   1  MiB  (1049000 bytes) =  15  seconds  (~69,905 bytes/second)
-  //
-  // For more information, see:
-  // https://metrics.torproject.org/torperf.html?start=2022-12-06&end=2023-03-06&server=onion
-  //
-  // The timeout returned is larger than the expected download time to account for
-  // network variability. We use 50,000 bytes/second instead of 69,905 bytes/second
-  // and apply a 1.5x adjustment factor.
-  //
-  // Minimum timeout is 25 seconds.
-  private getRealisticTimeout(size: bytes): ms {
-    const TIMEOUT_BYTES_PER_SECOND = 50_000.0;
-    const TIMEOUT_ADJUSTMENT_FACTOR = 1.5;
-    const TIMEOUT_BASE = 25_000; // 25 seconds in milliseconds
-
-    const timeout = Math.ceil(
-      (size / TIMEOUT_BYTES_PER_SECOND) *
-        TIMEOUT_ADJUSTMENT_FACTOR *
-        1000,
-    );
-    return (timeout + TIMEOUT_BASE) as ms;
   }
 
   // Queries the database for all items that need to be downloaded and queues
@@ -250,7 +223,7 @@ export class TaskQueue {
       timeout = MESSAGE_REPLY_DOWNLOAD_TIMEOUT_MS;
     } else {
       // Files use dynamic timeout based on size
-      timeout = this.getRealisticTimeout(metadata.size as bytes);
+      timeout = getRealisticTimeout(metadata.size as bytes);
     }
 
     console.debug(
