ec6e8f3b33b86d5f9f1650017479bbb4683b953a
Kunal Mehta <legoktm@debian.org>
2026-06-11 16:57:37 -0400

    Fix virtualenv creation in trixie
    pip now wants setuptools to be in the virtualenv for use to use
    `--no-use-pep517`, so install python3-setuptools. We already
    pass --system-site-packages to all the venvs we create, so
    let's always use it.
    
    The newer setuptools treats pyproject.toml, if it exists, as
    authoritative and ignores setup.py, so we need to move the necessary
    metadata into pyproject.toml.
    
    Unfortunately we need to keep stub setup.py files around for
    securedrop-builder's parsing; hopefully we can drop those soon.

--- export/setup.py
+++ export/setup.py
@@ -1,26 +1,4 @@
 import setuptools
 
-with open("README.md") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="securedrop-export",
-    version="0.0.0",
-    author="Freedom of the Press Foundation",
-    author_email="securedrop@freedom.press",
-    description="SecureDrop Qubes export scripts",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="GPLv3+",
-    url="https://github.com/freedomofpress/securedrop-export",
-    packages=setuptools.find_packages(exclude=["docs", "tests"]),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-    ],
-    entry_points={"console_scripts": ["send-to-usb = securedrop_export.main:entrypoint"]},
-)
+# Stub file for securedrop-builder
+setuptools.setup(name="securedrop-export")
--- log/setup.py
+++ log/setup.py
@@ -1,31 +1,4 @@
 import setuptools
 
-with open("README.md") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="securedrop-log",
-    version="0.0.0",
-    author="Freedom of the Press Foundation",
-    author_email="securedrop@freedom.press",
-    description="SecureDrop Qubes logging scripts",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="GPLv3+",
-    packages=setuptools.find_packages(exclude=["docs", "tests"]),
-    url="https://github.com/freedomofpress/securedrop-log",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Intended Audience :: Developers",
-        "Operating System :: POSIX :: Linux",
-    ],
-    entry_points={
-        "console_scripts": [
-            "securedrop-log-saver = log_server.log_saver:main",
-            "securedrop-redis-log = log_server.redis_log:main",
-        ]
-    },
-)
+# Stub file for securedrop-builder
+setuptools.setup(name="securedrop-log")
