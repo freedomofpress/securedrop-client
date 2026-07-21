4ee51bef7b67386d32d9b268a1ebf9c66587aed4
Cory Francis Myers <cory@freedom.press>
2026-07-21 10:35:12 -0700

    build: use corepack directly (no need for temporary directories inside the build container)

--- debian/build-app.sh
+++ debian/build-app.sh
@@ -2,21 +2,9 @@
 
 set -euxo pipefail
 
-COREPACK_HOME="$(mktemp -d)"
-COREPACK_SHIMS="$(mktemp -d)"
-export COREPACK_HOME
-export PATH="$COREPACK_SHIMS:$PATH"
-cleanup_corepack_home() {
-    rm -R "$COREPACK_HOME" "$COREPACK_SHIMS"
-}
-trap cleanup_corepack_home EXIT
-trap 'exit 129' HUP
-trap 'exit 130' INT
-trap 'exit 143' TERM
-
 cd app
 
-corepack enable --install-directory "$COREPACK_SHIMS"
+corepack enable
 corepack install
 
 pnpm install --frozen-lockfile
